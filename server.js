ee063623909376de8c5e3e217d58c3cc9d77d186
thaihoc2001 <thaihoc95x@gmail.com>
2021-08-23 22:56:22 +0700

    add socket io

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { origins: '*:*'});
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
@@ -24,7 +26,6 @@ mongoose.connect(process.env.MONGO_URL_LOCAL,{
     .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT,()=>{
     console.log(`App listen at http://localhost:${PORT}`)
 });
