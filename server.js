253f63b3e8dec0ff0d1ef83fdb5d9b6842edc291
thaihoc2001 <thaihoc95x@gmail.com>
2021-08-23 21:52:32 +0700

    delete websoket

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const http = require('http');
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.get('/',(req,res) => {
     res.send('Welcome to TH Blog service');
 });
-const { onServerInitialized } = require('./server/utils/app');
-
 
 const loginUser = require('./server/router/login.route');
 const confide = require('./server/router/confide.route');
@@ -26,9 +23,7 @@ mongoose.connect(process.env.MONGO_URL_LOCAL,{
     .then(()=> console.log('Mongodb connected'))
     .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-const server = http.createServer(app);
-onServerInitialized(server);
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
     console.log(`App listen at http://localhost:${PORT}`)
